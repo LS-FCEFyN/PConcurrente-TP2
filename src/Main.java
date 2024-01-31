5a0d269fcc8531fd793bbd500f124f7145724c32
LS-FCEFYN <loretta3087@hotmail.com>
2024-01-31 12:21:37 -0300

    Updated .gitignore Improved error handling for different scenarios when parsing arguments

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,38 @@ import java.io.IOException;
 public class Main {
   public static void main(String[] args) {
     try {
-      PetriNet petriNet = Parser.parseXmlFile("Test.pflow");
+      if (args.length > 0 && args[0].matches(".*\\.pflow")) {
+        PetriNet petriNet = Parser.parseXmlFile(args[0]);
 
-      for(int i = 0; i < args.length; i += 2) {
-        String id = args[i];
-        int firingRate = Integer.parseInt(args[i+1]);
-        petriNet.getTransitionById(id).ifPresent(TransitionUtils.combine(
-          t -> t.setFiringRate(firingRate), 
-          t -> t.setIsTimed(true)));
-    }
+        for (int i = 1; i < args.length; i += 2) {
+          String id = args[i];
 
-      petriNet.printPetriNetAscii();
+          if (i + 1 >= args.length || !args[i + 1].matches("\\d+")) {
+            System.out.println("Invalid argument format. " + 
+            "Usage: PETRI_NET.pflow TRANSITION_ID FIRING_RATE_IN_MS");
+            System.exit(0);
+          }
+
+          int firingRate = Integer.parseInt(args[i + 1]);
+
+          if(firingRate == 0) {
+            continue;
+          }
+
+          petriNet.getTransitionById(id).ifPresent(
+              TransitionUtils.combine(t -> t.setFiringRate(firingRate),
+                                      t -> t.setIsTimed(true)));
+        }
+
+        petriNet.printPetriNetAscii();
+      } else {
+        System.out.println(
+            "Usage: PETRI_NET.pflow or " + 
+            "PETRI_NET.pflow TRANSITION_ID FIRING_RATE_IN_MS");
+        System.exit(0);
+      }
     } catch (ParserConfigurationException | SAXException | IOException e) {
-      e.printStackTrace();
+      System.out.println(e.getClass().getName());
     }
   }
 }
